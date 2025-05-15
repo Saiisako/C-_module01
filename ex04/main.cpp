1aa01b024a9e9f193b9d459255574b8fde1e9a90
Sacha Kock <skock@c4r2p4.42nice.fr>
2025-05-15 14:55:40 +0200

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,29 +6,51 @@
 /*   By: skock <skock@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:09:34 by skock             #+#    #+#             */
-/*   Updated: 2025/05/07 17:30:51 by skock            ###   ########.fr       */
+/*   Updated: 2025/05/15 14:52:56 by skock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-int	main(int ac, char **av)
+void	append(std::ifstream& fs, std::ofstream& new_fs, char **av)
 {
-	std::fstream fs;
-	std::fstream new_fs;
-	std::stringstream name;
+	std::string	line, new_line, from, to;
 
-	name << av[1] << ".replace";
-	fs.open(av[1]);
-	if (!fs.is_open())
-		std::cout << "Error while trying to open file : " << av[1] << std::endl;
-	new_fs.open(name.str());
-	fs.close();
-	new_fs.close()
-	if (parse_arguments(ac))
-		return (print_error(), 1);
-	return (0);
-
+	from = (std::string(av[2]));
+	to = (std::string(av[3]));
+	while (getline(fs, line))
+	{
+		new_line = line;
+		size_t	pos = 0;
+		while ((pos = new_line.find(from, pos)) != std::string::npos)
+		{
+			new_line.erase(pos, from.length());
+			new_line.insert(pos, to);
+			pos += to.length();
+		}
+		new_fs << new_line << std::endl;
+	}
+	
 }
 
-(*f)(int)
+int	main(int ac, char **av)
+{
+	std::ifstream fs(av[1]);
+
+	std::stringstream name;
+	name << av[1] << ".replace";
+	std::ofstream new_fs(name.str().c_str());
+	if (parse_arguments(ac))
+		return (print_error(), 1);
+	if (fs.is_open())
+		{
+			if (new_fs.is_open())
+				append(fs, new_fs, av);
+			else
+				std::cout << "Error while trying to open file : " << av[1] << std::endl;
+		}
+	else
+		std::cout << "Error while trying to open file : " << av[1] << std::endl;
+	fs.close();
+	return (0);
+}
--- ex04/parsing.cpp
+++ ex04/parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: skock <skock@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 12:24:51 by skock             #+#    #+#             */
-/*   Updated: 2025/05/07 12:26:30 by skock            ###   ########.fr       */
+/*   Updated: 2025/05/15 13:53:27 by skock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	parse_arguments(int ac)
 {
-	if (ac == 4)
-		return (0);
-	else
+	if (ac < 4 || ac > 4)
 		return (1);
+	else
+		return (0);
 }
 
 void	print_error() {std::cout << "Enter : [1] = filename | [2] = string_1 | [3] = string_2" << std::endl;}
